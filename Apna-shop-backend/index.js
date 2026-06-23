95bcaba04bcf2d867aa826b86a114a7f27771af3
Shivam kumar jha <shivamkumarjha961@gmail.com>
2026-06-23 14:53:53 +0530

    Add Vercel serverless configurations for backend

--- Apna-shop-backend/index.js
+++ Apna-shop-backend/index.js
@@ -23,3 +23,5 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/wishlist", wishlistRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
